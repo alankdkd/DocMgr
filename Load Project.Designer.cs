f7178ccde5253c842fd85f8fc3101c7e72d91a9e
Alan Balkany <alankdkd@aol.com>
2023-05-03 12:28:12 -0400

    Added Load Project.

---
+++ Load Project.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace DocMgr
+{
+    partial class LoadProjectDlg
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonSelect = new System.Windows.Forms.Button();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 60);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Project";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(68, 120);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(441, 554);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // buttonSelect
+            // 
+            this.buttonSelect.Enabled = false;
+            this.buttonSelect.Location = new System.Drawing.Point(534, 120);
+            this.buttonSelect.Margin = new System.Windows.Forms.Padding(5);
+            this.buttonSelect.Name = "buttonSelect";
+            this.buttonSelect.Size = new System.Drawing.Size(135, 65);
+            this.buttonSelect.TabIndex = 2;
+            this.buttonSelect.Text = "&Select";
+            this.buttonSelect.UseVisualStyleBackColor = true;
+            this.buttonSelect.Click += new System.EventHandler(this.buttonSelect_Click);
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Location = new System.Drawing.Point(905, 123);
+            this.buttonBrowse.Margin = new System.Windows.Forms.Padding(5);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(135, 65);
+            this.buttonBrowse.TabIndex = 3;
+            this.buttonBrowse.Text = "&Browse...";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(905, 220);
+            this.buttonClose.Margin = new System.Windows.Forms.Padding(5);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(135, 65);
+            this.buttonClose.TabIndex = 4;
+            this.buttonClose.Text = "&Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // LoadProjectDlg
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1257, 750);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.buttonSelect);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "LoadProjectDlg";
+            this.Text = "LoadProject";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private ListBox listBox1;
+        private Button buttonSelect;
+        private Button buttonBrowse;
+        private Button buttonClose;
+    }
+}
---
+++ Load Project.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace DocMgr
+{
+    public partial class LoadProjectDlg : Form
+    {
+        private Dictionary<string, string> projMap = new Dictionary<string, string>();
+        public string selectedPath = "";
+
+        public LoadProjectDlg()
+        {
+            InitializeComponent();
+
+            ReadPreviousProjects();
+            AddProjectsToListBox();
+            listBox1.Enabled = projMap.Count > 0;
+            DialogResult = DialogResult.Cancel;
+        }
+
+        private void AddProjectsToListBox()
+        {
+            foreach (string projName in projMap.Keys)
+                listBox1.Items.Add(projName);
+        }
+
+        private void ReadPreviousProjects()
+        {
+            int numProjects = GetNumProjects();
+
+            for (int i = 0; i < numProjects; i++)
+                ReadNextProject(i);
+        }
+
+        private void ReadNextProject(int i)
+        {
+            RegistryKey? key;
+            string projIndex = "Project" + i;
+            key = Registry.CurrentUser.OpenSubKey(@"Software\PatternScope Systems\DocMgr\"
+                + projIndex, RegistryKeyPermissionCheck.ReadWriteSubTree);
+
+            if (key == null)
+                return;
+
+            string projName = key.GetValue("Name").ToString();
+            string projPath = key.GetValue("Path").ToString();
+            projMap[projName] = projPath;
+        }
+
+        private static int GetNumProjects()
+        {
+            RegistryKey? key = Registry.CurrentUser.OpenSubKey(
+                @"Software\PatternScope Systems\DocMgr",
+                RegistryKeyPermissionCheck.ReadWriteSubTree);
+
+            if (key == null)
+                return 0;           // Taint none!
+
+            object? obj = key.GetValue("NumProjects");
+
+            if (obj == null)
+                return 0;           // Taint none!
+
+            return (int)obj;
+        }
+        public static void AddProject(string name, string path)
+        {
+            int numProjects = GetNumProjects();
+            string projIndex = "Project" + numProjects++;
+
+            RegistryKey? key = Registry.CurrentUser.CreateSubKey(
+                @"Software\PatternScope Systems\DocMgr\" + projIndex,
+                RegistryKeyPermissionCheck.ReadWriteSubTree);
+
+            if (key != null)
+            {
+                key.SetValue("Name", name);
+                key.SetValue("Path", path);
+                key = Registry.CurrentUser.OpenSubKey(
+                   @"Software\PatternScope Systems\DocMgr",
+                   RegistryKeyPermissionCheck.ReadWriteSubTree);
+                key.SetValue("NumProjects", numProjects);
+            }
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            buttonSelect_Click(sender, e);
+        }
+
+        private void buttonSelect_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1)
+                return;
+
+            string projName = listBox1.SelectedItem.ToString();
+            selectedPath = projMap[projName];
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            string? projectPath = DocMgr.SelectFile("json files (*.json)|*.json|All files (*.*)|*.*");
+            RegistryKey? key;
+
+            if (projectPath != null)
+            {                                               // Project file selected:
+                selectedPath = projectPath.ToString();
+                key = Registry.CurrentUser.CreateSubKey(
+                    @"Software\PatternScope Systems\DocMgr",
+                    RegistryKeyPermissionCheck.ReadWriteSubTree);
+                key.SetValue("LastProjectPath", projectPath.ToString());
+                DialogResult = DialogResult.OK;
+                Close();
+            }
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+            buttonSelect.Enabled = true;
+        }
+    }
+}
